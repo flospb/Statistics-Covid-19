f5952247585bb99bd08fa9138f6c4de0908c840a
flospb <flospb93@gmail.com>
2021-08-08 12:54:21 +0300

    Создание и первичная настройка проекта, добавление файла gitignore

---
+++ Statistics Covid-19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Statistics Covid-19
+//
+//  Created by Сергей Флоря on 08.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

d89c64f7e03c518b92ea8336d0699f909355eddc
flospb <flospb93@gmail.com>
2021-08-11 23:09:35 +0300

    Экран информации, добавлен список симптомов (тестовый вариант, еще в работе)

---
+++ Statistics Covid-19/Presentation layer/Information/InformationModels.swift
@@ -0,0 +1,41 @@
+//
+//  InformationModels.swift
+//  Statistics Covid-19
+//
+//  Created by Сергей Флоря on 09.08.2021.
+//
+
+import Foundation
+
+struct SymptomsArray {
+    var symptomsArray = [Symptom]()
+    
+    init() {
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+        symptomsArray.append(Symptom(title: "Оставайся дома", content: "Карантин - Способ остановить распространение коронавируса"))
+    }
+}
+
+struct Symptom {
+    var title: String
+    var content: String
+}
